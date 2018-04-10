4a830bd83453e6ff2b18b34b74fa812817df2414
d46 <daghangunay@gmail.com>
2018-04-10 12:31:19 +0300

    feat: angular cli

--- nvm.sh
+++ nvm.sh
@@ -2,4 +2,8 @@ brew install nvm
 [[ -s $(brew --prefix)/opt/nvm/nvm.sh ]] && . $(brew --prefix)/opt/nvm/nvm.sh
 nvm install 8.5
 node -v
+
+# Global packages
 npm install -g yarn
+npm install -g @angular/cli
+ng set --global packageManager=yarn
